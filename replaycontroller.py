6b41a8740316beb451485462693bd098cc3ad81e
Florian L <florian.luettgens@gmail.com>
2018-07-20 21:38:47 +0200

    Update replaycontroller.py
    fullscreen

--- replaycontroller.py
+++ replaycontroller.py
@@ -1,6 +1,7 @@
 # can control the sc2 replay window, obsolete with current replay overlay
 
 import win32ui
+import win32gui
 import keyboard
 import time
 
@@ -14,22 +15,26 @@ def WindowExists(classname):
 
 start = 0
 sendcommands = False
+fullscreen = False
 
 while True:
 	if WindowExists("StarCraft II"):
 		if not (start):
+			handle = win32gui.FindWindow(0, "StarCraft II")
+			win32gui.BringWindowToTop(handle)
+			win32gui.SetForegroundWindow(handle)
 			start = time.time()
 		
 		now = time.time()
 		timer = int(now - start)
-
+		if ((timer > 10) and (fullscreen == False)):
+			keyboard.press_and_release('alt+enter')
+			fullscreen = True
+		
 		if ((timer > 30) and (sendcommands == False)):
-			# lower the time-bar
-			keyboard.press_and_release('control+t')
-			time.sleep(0.2)
-			# show the APM overlay
-			keyboard.press_and_release('control+c')
+			keyboard.press_and_release('d')
 			sendcommands = True
 	else:
 		start = 0
 		sendcommands = False
+		fullscreen = False
